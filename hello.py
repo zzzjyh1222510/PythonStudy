380d673110531c5285b2d8d520c4d2ad786c7bea
zzzjyh1222510 <zzzjyh122510@163.com>
2020-12-25 15:20:35 +0800

    first up

---
+++ hello.py
@@ -0,0 +1,43 @@
+# print("你好")   # 字符串 | '#' - 注释
+# print(233)      # 整数
+# print(233.33)   # 小数
+# print(True)     # 布尔值
+# print(())       # 元组
+# print([])       # 集合
+# print({})       # 字典
+
+# print()打印多个值
+# print("XXX",233)
+
+# 字符串操作
+# print("hello"+"world") # 字符串的拼接 - '+'
+# print("hello"*5)  # 打印多遍 - "字符串"*N
+
+# 布尔值判断操作
+# print(2<3)
+
+# # 变量
+# # 赋值
+# a="hello"
+# # 把"hello"这个值赋值给了名字叫a的这个变量
+# #变量名要小写，且不能含有特殊符号
+
+# #input() - 获取输入
+# c=100
+# d=input("请输入d = ")
+# print(type(d))
+# # 类型转换
+# d=int(d)
+# print(type(d))
+# print(c*d)
+
+# 计算字符串,集合类型数据长度
+# print(len("abcd"))
+
+"""
+练习：通过代码获取两段内容，并且计算他们长度的和
+"""
+# a = input("a = ")
+# b = input("b = ")
+# print(len(a+b))
+
